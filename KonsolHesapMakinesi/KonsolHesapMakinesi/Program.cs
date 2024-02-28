e2a6c5bba2cbe599f805a29a3075350c3ab128e1
Yiğit Berk <128992676+Yigit-Berk@users.noreply.github.com>
2024-02-28 22:57:23 +0300

    Add files via upload

---
+++ KonsolHesapMakinesi/KonsolHesapMakinesi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolHesapMakinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=8,b=10;
+
+            Console.WriteLine("Toplama:"+(a+b));
+            Console.WriteLine("Çıkarma:"+(a-b));
+            Console.WriteLine("Çarpma:"+(a*b));
+            Console.WriteLine("Bölme:"+(a/b));
+
+            Console.ReadKey();
+        }
+
+        /*Parametreli fonksiyon ile yapımı:*/
+    }
+}
